1d5bb52172e6326ed16b89ccc363cbc55bfd969e
Marta <mrtdeneke@gmail.com>
2022-04-18 14:27:12 -0700

    finished portfolio

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -37,8 +37,7 @@ class Experience extends Component {
               color: "#fff",
               textAlign: "center",
             }}
-            icon={<i className="fab fa-angular experience-icon"></i>}
-            key={i}
+          
           >
             <div style={{ textAlign: "left", marginBottom: "4px" }}>
               {mainTech}
